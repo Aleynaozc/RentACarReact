8fab48473cdcb77c3d05546d3a8e4514667cfda0
Aleynaozc <aleynaozcelik48@gmail.com>
2022-05-24 15:35:11 +0300

    added-fontawesome

--- src/components/cars.js
+++ src/components/cars.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../assets/styles/components/car.css';
+import "../assets/styles/components/car/style.css"
 
 function cars() {
   return (
     <div className="container-fluid">
         <div className="row">
             <div className="car__card">
-                <div className=" col-lg-4 col-md-5  card" style={{background:"black"}} >
+                <div className=" col-lg-4 col-md-5  card">
                    
-                    <p className="car__classNameification ">EKONOMİK</p> 
+                    <p className="car__classification ">EKONOMİK</p> 
                         
                     <p className="car_name">FIAT EGEA</p>         
                     <div className="slider">
@@ -19,13 +19,13 @@ function cars() {
                     </div>   
                    
                   
-                    <img className="card-img-top" src="/"  alt="Card image cap"/> 
+                    <img className="card-img-top" src={require('../assets/images/araba1.jpg')}  alt="Card image cap"/> 
                 </div>
                     <div className="card-body">
                         <div className="card__features">
                             <i className="fa-solid fa-gas-pump mt-1"></i>
                             <p>Disel</p>
-                               <img src="/src/assets/images/bg.jpg" className="mt-1"/>
+                            <img src={require('../assets/images/transmission.png')} className="mt-1 transmission__image"/>
                            <p>Manual</p>
                            <i className="fa-solid fa-credit-card mt-1"></i>
                            <p>Credit Card</p>
@@ -43,21 +43,21 @@ function cars() {
                 </div>
 
 
-                <div className=" col-lg-4 col-md-5  card" >
-                    <p className="car__classNameification ">KONFOR</p> 
+                <div className="col-lg-4 col-md-5  card" >
+                    <p className="car__classification ">KONFOR</p> 
                     <p className="car_name">BMW 2 SERİSİ</p> 
                     <div className="slider">
                     <div className="slider__arrow">
                         <i className="fa-solid fa-circle-arrow-left arrows" ></i>
                         <i className="fa-solid fa-circle-arrow-right arrows" ></i>
                     </div>
-                    <img className="card-img-top" src="assets/images/bmw2.png" alt="Card image cap"/> 
+                    <img className="card-img-top" src={require('../assets/images/bmw2.png')} alt="Card image cap"/> 
                 </div>
                     <div className="card-body">
                         <div className="card__features">
                             <i className="fa-solid fa-gas-pump mt-1"></i>
                             <p>Disel</p>
-                               <img src="assets/images/manual-transmission.png"  className="mt-1"/>
+                               <img src={require('../assets/images/transmission.png')} className="mt-1 transmission__image"/>
                            <p>Manual</p>
                            <i className="fa-solid fa-credit-card mt-1"></i>
                            <p>Credit Card</p>
@@ -72,20 +72,20 @@ function cars() {
                     </div>
                 </div>
                 <div className=" col-lg-4 col-md-5  card" >
-                    <p className="car__classNameification ">PRESTIJ</p> 
+                    <p className="car__classification ">PRESTIJ</p> 
                     <p className="car_name">AUDI Q8</p>   
                     <div className="slider">
                     <div className="slider__arrow">
                         <i className="fa-solid fa-circle-arrow-left arrows" ></i>
                         <i className="fa-solid fa-circle-arrow-right arrows" ></i>
                     </div> 
-                    <img className="card-img-top" src="" alt="Card image cap"/>
+                    <img className="card-img-top" src={require('../assets/images/audi-Q8.jpg')} alt="Card image cap"/>
                 </div>
                     <div className="card-body">
                         <div className="card__features">
                             <i className="fa-solid fa-gas-pump mt-1"></i>
                             <p>Disel</p>
-                               <img src='/src/assets/images/bmw2.png' className="mt-1"/>
+                            <img src={require('../assets/images/transmission.png')} className="mt-1 transmission__image"/>
                            <p>Manual</p>
                            <i className="fa-solid fa-credit-card mt-1"></i>
                            <p>Credit Card</p>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
