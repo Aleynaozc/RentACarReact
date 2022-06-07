19bcabd8fa3bbe8f1927374cb4c8877551662570
Aleynaozc <aleynaozcelik48@gmail.com>
2022-06-07 18:05:38 +0300

    added-category-filter

--- src/containers/Reservation/CarList.js
+++ src/containers/Reservation/CarList.js
@@ -40,123 +40,135 @@ function cars() {
     }, []);
 
     //FILTERED
-    const [selectedCategory, setSelectedCategory] = useState();
-    
-    
-    
-     function getFilteredList() {
-        if (!selectedCategory ) {
-          return carList
-        }
-       
-        return   carList.filter((item) => item.transmissionType.type === selectedCategory || item.fuelType.type === selectedCategory || item.classification.type === selectedCategory );
-      }
-      var filteredList = useMemo(getFilteredList, [selectedCategory, carList]);
-
-     
+    const [selectedCategory, setSelectedCategory] = useState({
+        transmissionType: [],
+        fuelType: [],
+        classification: [],
+    });
 
 
-      const [filterSearch, setFilterSearch] = useState('');
+    
+    function getFilteredList() {
+        let testList = carList;
+        console.log(selectedCategory)
+        Object.keys(selectedCategory).forEach(catType => {
+            if (selectedCategory[catType].length > 0) {
+                testList = testList.filter(car => {
+                    if (selectedCategory[catType].findIndex(sc => sc == car[catType].type) != -1) return car
+                });
+            }
+        })
+        // if (!selectedCategory) {
+        //     return carList
+        // }
+        // return carList.filter((item) => item.transmissionType.type === selectedCategory || item.fuelType.type === selectedCategory || item.classification.type === selectedCategory);
 
-      // the search result
-    
-    
-      const filter = (e) => {
+        return testList;
+    }
+    var filteredList = useMemo(getFilteredList, [selectedCategory, carList]);
+
+
+
+
+    const [filterSearch, setFilterSearch] = useState('');
+
+    // the search result
+    const filter = (e) => {
         const keyword = e.target.value;
-    
+
         if (keyword !== '') {
-          const results = carList.filter((user) => {
-            return user.transmissionType.type.toLowerCase().startsWith(keyword.toLowerCase())
-            || user.fuelType.type.toLowerCase().startsWith(keyword.toLowerCase())
-            || user.fuelType.type.toLowerCase().startsWith(keyword.toLowerCase())
-            || user.classification.type.toLowerCase().startsWith(keyword.toLowerCase())
-            || user.brand.name.toLowerCase().startsWith(keyword.toLowerCase()) ;
-           
-          });
-          setCarList(results);
+            const results = carList.filter((user) => {
+                return user.transmissionType.type.toLowerCase().startsWith(keyword.toLowerCase())
+                    || user.fuelType.type.toLowerCase().startsWith(keyword.toLowerCase())
+                    || user.fuelType.type.toLowerCase().startsWith(keyword.toLowerCase())
+                    || user.classification.type.toLowerCase().startsWith(keyword.toLowerCase())
+                    || user.brand.name.toLowerCase().startsWith(keyword.toLowerCase());
+
+            });
+            setCarList(results);
         } else {
-          getCars();
-         
+            getCars();
+
         }
-    
+
         setFilterSearch(keyword);
-      };
+    };
 
 
- 
-  
+
+
     return (
         <div>
             <div className='row'>
-            <div className='search__button_area'>
-            <input
-        type="search"
-        value={filterSearch}
-        onChange={filter}
-        className="search__button"
-        placeholder="Seacrh"
-      />
-            </div>
-         
-        
-            <Category selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-             
-            <div className="col-lg-8 col-md-8  car__card">
-            
-                <div className='row'>
-          
-                    {
-                        filteredList .map((carItem, index) => {
+                <div className='search__button_area'>
+                    <input
+                        type="search"
+                        value={filterSearch}
+                        onChange={filter}
+                        className="search__button"
+                        placeholder="Seacrh"
+                    />
+                </div>
 
-                            return <>
-                            
-                            <div className="card" key={index}>
-                                
-                                    <p className="car__classification">{carItem.classification.type}</p>
-                                    <p className="car_name">{carItem.brand.name} {carItem.carModal.name} </p>
-                                   
-                                    <div className="slider__arrow__container col-lg-6">
-                                        <div className='slider__arrow'>
-                                            <i className="fa-solid fa-circle-arrow-left arrows" ></i>
-                                            <i className="fa-solid fa-circle-arrow-right arrows" ></i>
+
+                <Category selectedCategory={selectedCategory} setCarList={setCarList} setSelectedCategory={setSelectedCategory} />
+
+                <div className="col-lg-8 col-md-8  car__card">
+
+                    <div className='row'>
+
+                        {
+                            filteredList.map((carItem, index) => {
+
+                                return <>
+
+                                    <div className="card" key={index}>
+
+                                        <p className="car__classification">{carItem.classification.type}</p>
+                                        <p className="car_name">{carItem.brand.name} {carItem.carModal.name} </p>
+
+                                        <div className="slider__arrow__container col-lg-6">
+                                            <div className='slider__arrow'>
+                                                <i className="fa-solid fa-circle-arrow-left arrows" ></i>
+                                                <i className="fa-solid fa-circle-arrow-right arrows" ></i>
+                                            </div>
+                                        </div>
+                                        <div className='row mb-3'>
+                                            <div className='slider__container col-lg-6'>
+                                                <div className="slider ">
+                                                    <img className="card-img-top" src={carItem.imgURL} alt="Card image cap" />
+                                                </div>
+                                            </div>
+
+                                            <div className="card__features-body  col-lg-6 col-md-12 col-sm-10">
+                                                <div className="card__features ">
+                                                    <i className="fa-solid fa-gas-pump icons "></i>
+                                                    <p className='features'>{carItem.fuelType.type}</p>
+                                                    <img className="mt-1 transmission__image" src={'../images/transmission.png'} />
+                                                    <p className='features'>{carItem.transmissionType.type} </p>
+                                                    <i className="fa-solid fa-credit-card icons"></i>
+                                                    <p className='features'>Credit Card</p>
+                                                </div>
+                                                <div className="car__price">
+
+                                                    <span className=" total__price">{diffInDays === 0 ? carItem.price : carItem.price * diffInDays} TL </span>
+
+                                                    <span className="daily__price">{carItem.price} / Daily</span>
+                                                </div>
+                                                <div className="pay__button-area">
+                                                    <button className="pay__button">Pay Now</button>
+                                                </div>
+                                            </div>
+
                                         </div>
                                     </div>
-                                    <div className='row mb-3'>
-                                    <div className='slider__container col-lg-6'>
-                                    <div className="slider ">
-                                            <img className="card-img-top" src={carItem.imgURL} alt="Card image cap" />
-                                    </div>
-                                    </div>
+                                </>
+                            })}
+                    </div>
 
-                                    <div className="card__features-body  col-lg-6 col-md-12 col-sm-10">
-                                        <div className="card__features ">
-                                            <i className="fa-solid fa-gas-pump icons "></i>
-                                            <p className='features'>{carItem.fuelType.type}</p>
-                                            <img className="mt-1 transmission__image" src={'../images/transmission.png'} />
-                                            <p className='features'>{carItem.transmissionType.type} </p>
-                                            <i className="fa-solid fa-credit-card icons"></i>
-                                            <p className='features'>Credit Card</p>
-                                        </div>
-                                        <div className="car__price">
-
-                                            <span className=" total__price">{diffInDays===0 ? carItem.price : carItem.price * diffInDays} TL </span>
-
-                                            <span className="daily__price">{carItem.price} / Daily</span>
-                                        </div>
-                                        <div className="pay__button-area">
-                                            <button className="pay__button">Pay Now</button>
-                                        </div>
-                                    </div>
-                                
-                                </div>
-                           </div>
-                            </>
-                        })}
                 </div>
 
             </div>
-
-        </div>
         </div>
 
     )
--- src/containers/Reservation/Category.js
+++ src/containers/Reservation/Category.js
@@ -2,18 +2,26 @@
 
 import React from 'react'
 
-function Category({setSelectedCategory}) {
+function Category({ setSelectedCategory, setCarList, selectedCategory }) {
 
-
-    function handleCategoryChange(event) { 
-        if (event.target.checked) {
-            setSelectedCategory(event.target.value);
-        } else {
-            setSelectedCategory(null);
+    const setCategroyFilters = (value, type) => {
+        const findedItemIndex = selectedCategory[type].findIndex(x => x == value);
+        if (findedItemIndex == -1)
+            setSelectedCategory({ ...selectedCategory, [type]: [...selectedCategory[type], value] });
+        else {
+            selectedCategory[type].splice(findedItemIndex, 1)
+            setSelectedCategory({
+                ...selectedCategory,
+                type: [...selectedCategory[type]]
+            });
         }
-     }
- 
-  
+    }
+
+    function handleCategoryChange(event, type) {
+        setCategroyFilters(event.target.value, type)
+    }
+
+
     return (
 
         <div className='col-lg-4 col-md-4 filter__container'>
@@ -24,13 +32,13 @@ function Category({setSelectedCategory}) {
                     <div className='categories__item'>
                         <h5>Transmission Type</h5>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom'  value='Automatic'    onChange={ handleCategoryChange }  />
+                            <input type="checkbox" className='custom' value='Automatic' onChange={(e) => { handleCategoryChange(e, "transmissionType") }} />
                             <span className="form-check-label" for="flexCheckDefault"  >
                                 Automatic
                             </span>
                         </div>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Manual' onChange={handleCategoryChange} />
+                            <input type="checkbox" className='custom' value='Manual' onChange={(e) => { handleCategoryChange(e, "transmissionType") }} />
                             <label className="form-check-label" for="flexCheckChecked">
                                 Manual
                             </label>
@@ -39,48 +47,48 @@ function Category({setSelectedCategory}) {
                     <div className='categories__item'>
                         <h5>Fuel Type</h5>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Gasoline' onChange={handleCategoryChange} />
+                            <input type="checkbox" className='custom' value='Gasoline' onChange={(e) => { handleCategoryChange(e, "fuelType") }} />
                             <label className="form-check-label" for="flexCheckDefault">
                                 Gasoline
                             </label>
                         </div>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Diesel' onChange={handleCategoryChange}  />
+                            <input type="checkbox" className='custom' value='Diesel' onChange={(e) => { handleCategoryChange(e, "fuelType") }} />
                             <label className="form-check-label" for="flexCheckChecked">
                                 Diesel
                             </label>
                         </div>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Lpg' onChange={handleCategoryChange} />
+                            <input type="checkbox" className='custom' value='Lpg' onChange={(e) => { handleCategoryChange(e, "fuelType") }} />
                             <label className="form-check-label" for="flexCheckChecked">
                                 Lpg
                             </label>
                         </div>
                     </div>
-                    <div className='categories__item'>
+                    {/* <div className='categories__item'>
                         <h5>Classification</h5>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Economic' onChange={handleCategoryChange}  />
+                            <input type="checkbox" className='custom' value='Economic' onChange={handleCategoryChange("classification")}  />
                             <label className="form-check-label" for="flexCheckDefault">
                                 Economic
                             </label>
                         </div>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Standart' onChange={handleCategoryChange} />
+                            <input type="checkbox" className='custom' value='Standart' onChange={handleCategoryChange("classification")} />
                             <label className="form-check-label" for="flexCheckChecked">
                                 Standart
                             </label>
                         </div>
                         <div className="item__form-check">
-                            <input type="checkbox" className='custom' value='Premium' onChange={handleCategoryChange}  />
+                            <input type="checkbox" className='custom' value='Premium' onChange={handleCategoryChange("classification")}  />
                             <label className="form-check-label" for="flexCheckChecked">
                                 Premium
                             </label>
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
-               
+
             </div>
         </div>
     )
